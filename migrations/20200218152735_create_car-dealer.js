093484cb1ec40562fecfa07612c84188e8cd2db1
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-18 15:42:03 -0800

    completed schema for table 'cars'

--- migrations/20200218152735_create_car-dealer.js
+++ migrations/20200218152735_create_car-dealer.js
@@ -1,8 +1,24 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", table => {
+
+    table.increments();
+
+    table
+        .string("VIN", 32)
+        .notNullable()
+        .index();
+    
+    table.string("make", 32);
+    table.string("model", 32);
+    table.integer("mileage", 8);
+    table.string("transmission", 32);
+    table.string("titleStatus", 32);
+
+  });
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("cars");
 };
