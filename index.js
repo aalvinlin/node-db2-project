915471886564f96ea61d0332e4bc5e4544387af1
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-18 16:14:20 -0800

    POST and PUT implemented for table 'cars'

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ server.get("/api/cars", (req, res) => {
             res.status(500).json({message: "GET /api/cars failed."});
         })
     
-})
+});
 
 server.get("/api/cars/:id", (req, res) => {
 
@@ -41,7 +41,48 @@ server.get("/api/cars/:id", (req, res) => {
             res.status(500).json({message: "GET /api/cars/:id failed."});
         })
     
-})
+});
+
+server.post("/api/cars", (req, res) => {
+
+    if (!req.body || !req.body.vin)
+        { res.status(400).json({message: "VIN is required."}) }
+    
+    database("cars")
+    .insert(req.body)
+        .then(response => {
+            console.log("POST /api/cars response:", response);
+
+            if (response.length > 0)
+                { res.status(201).json(response); }
+            else
+                { res.status(404).json({message: "Couldn't POST to /api/cars"}); }
+        })
+        .catch(error => {
+            console.log("POST /api/cars error:", error);
+            res.status(500).json({message: "Couldn't POST to /api/cars"});
+        })
+});
+
+
+server.put("/api/cars/:id", (req, res) => {
+
+    database("cars")
+    .where({id: req.params.id})
+    .update(req.body)
+        .then(numberUpdated => {
+            console.log("PUT /api/cars/:id response:", numberUpdated);
+
+            if (numberUpdated > 0)
+                { res.status(200).json(numberUpdated); }
+            else
+                { res.status(404).json({message: "No car with ID " + req.params.id + " found."}); }
+        })
+        .catch(error => {
+            console.log("PUT /api/cars/:id error:", error);
+            res.status(500).json({message: "Couldn't PUT to /api/cars/:id"});
+        })
+});
 
 const port = 5000;
 
